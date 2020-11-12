916001cf27e1c7b0f52e9b05fca3622a533e6fb0
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2020-11-12 20:09:11 -0300

    Apply node taint default effect

--- app/controllers/node/controller.py
+++ app/controllers/node/controller.py
@@ -3,6 +3,7 @@ import json
 import copy
 from controllers import log
 
+
 def reconcile(state, config, *args):
     metadata = state.get('object',{}).get('metadata',{})
     annotations = metadata.get('annotations',{})
@@ -34,11 +35,13 @@ def reconcile(state, config, *args):
         labels[name] = value
         changed = True
 
+    # copy taints from labels and annotations
     new_taints = copy.deepcopy(node_taints)
     new_taints = reconcile_taints(labels, new_taints)
     new_taints = reconcile_taints(annotations, new_taints)
 
     if new_taints != node_taints:
+        log('Nothing to do')
         changed = True
 
     if not changed:
@@ -49,6 +52,7 @@ def reconcile(state, config, *args):
     state['object']['metadata']['labels'] = labels
 
     return state
+
 
 def reconcile_taints(source, node_taints):
     '''Taints from labels or annotations
@@ -61,6 +65,7 @@ def reconcile_taints(source, node_taints):
     new_taints = {}
     default_effect = os.environ.get('DEFAULT_NODE_TAINT_EFFECT', 'NoSchedule')
 
+    # read all taints from source
     for prefix_name, value in source.items():
         if not prefix_name.startswith('taint.getup.io/'):
             continue
@@ -72,21 +77,29 @@ def reconcile_taints(source, node_taints):
 
         new_taints[key][field] = value
 
+    # filter for taints to add to node
     taints_to_add = []
     for new_taint in new_taints.values():
         found = False
+
+        if 'effect' not in new_taint:
+            new_taint['effect'] = default_effect
+
         for node_taint in node_taints:
             if equal_taints(node_taint, new_taint):
                 log('Already exists taint: {}'.format(new_taint))
                 found = True
+
         if not found:
             taints_to_add.append(new_taint)
 
+    # add filtered taints
     for taint in taints_to_add:
         log('Adding taint: {}'.format(taint))
         node_taints.append(taint)
 
     return node_taints
+
 
 def equal_taints(a, b):
     eq = False
